00ad5e4125d321f6a626ecdfaa41bdd86ddea845
carol.raquel <carol.raquel.92@hotmail.com>
2020-03-07 20:47:39 -0300

    First commit

---
+++ Models/Genero.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Locadora.Models
+{
+    public class Genero
+    {
+        public int ID { get; set; }
+        public String Nome { get; set; }
+    }
+}
